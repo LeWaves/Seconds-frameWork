5541e9f87cd2de2a5ba1904138bc771a228acc1d
lewaves@yeah.net <litao@optaim.com>
2018-03-19 16:30:17 +0800

    adjust add MultipleTextViewGroup view

--- app/src/main/java/com/lt/integrate/frame/demo/HomeActivity.java
+++ app/src/main/java/com/lt/integrate/frame/demo/HomeActivity.java
@@ -5,18 +5,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.lt.integrate.frame.view.MultipleTextViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by iclick on 2017/9/30.
  */
 
 public class HomeActivity extends AppCompatActivity {
-
+    private MultipleTextViewGroup textViewGroup;
+    private List<String> groupList = new ArrayList<>();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity_layout);
+        initData();
+        initView();
+    }
 
+    private void initView() {
+        textViewGroup = (MultipleTextViewGroup) findViewById(R.id.textViewGroup);
+        textViewGroup.setMax_lins(1);
+        textViewGroup.setTextViews(groupList);
+    }
+
+    private void initData() {
+        groupList.add("网红美女");
+        groupList.add("青春美女");
+        groupList.add("素颜美女");
+        groupList.add("混血美女");
+        groupList.add("日韩美女");
+        groupList.add("日韩大战美女");
     }
 
     public void onHttpRequest(View view){
